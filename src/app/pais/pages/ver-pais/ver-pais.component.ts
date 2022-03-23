f97d6e7d0474c85397006e3b0435db74ef456dac
luis vallejo <lvallejo@externos.bancokestado.cl>
2022-03-23 19:57:48 -0300

    Terminada la primera parte de la aplicacion

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs/operators';
+import { Country } from '../../interfaces/pais-interfaces';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class VerPaisComponent implements OnInit {
+  pais!: Country;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private paisService: PaisService
+  ) {}
 
   ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({ id }) => this.paisService.getPaisPorAlpha(id)),
+        tap(console.log)
+      )
+      .subscribe((pais) => (this.pais = pais));
   }
-
 }
